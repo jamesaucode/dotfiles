63c5d588fafbc489639315349627e7c8900a8096
James Au <auchu@amazon.com>
2021-04-22 17:30:47 -0700

    Changed theme and font, use writeroom mode by default in org

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -17,10 +17,10 @@
 ;; font string. You generally only need these two:
 ;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
 ;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
-(setq doom-font (font-spec :family "FiraCode Nerd Font" :size 16 :weight 'regular)
-      doom-variable-pitch-font (font-spec :family "FiraCode Nerd Font" :size 17))
+(setq doom-font (font-spec :family "Iosevka Nerd Font" :size 18 :weight 'light)
+      doom-variable-pitch-font (font-spec :family "Iosevka Nerd Font" :size 20))
 
-(setq doom-theme 'doom-acario-dark)
+(setq doom-theme 'doom-tomorrow-night)
 
 ;; Always use ~/org just to be consistent, symlink it to ~/org if it's somewhere else
 (setq org-directory "~/org")
@@ -44,6 +44,7 @@
 (add-hook 'js2-mode-hook 'prettier-js-mode)
 (add-hook 'web-mode-hook 'prettier-js-mode)
 (add-hook 'typescript-mode-hook 'prettier-js-mode)
+(add-hook 'org-mode-hook 'writeroom-mode)
 ;; Some performance enhancement for magit
 (setq magit-git-executable "/usr/local/bin/git")
 (setq vc-handled-backends nil)
